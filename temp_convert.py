8518cbfec81a57a59b6ad0222014b688ac322cd6
Tony Tran <tony.tran1@stu.ocsb.ca>
2023-11-22 18:00:46 +0000

    Added temp_convert.py

---
+++ temp_convert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Created By: Tony Tran
+# Date: Nov. 22, 2023
+# This program will show you the conversation of Celsius to Fahrenheit.
+
+
+def fahrenheit():
+    user_temp = input("Enter the temperature (°C): ")
+    try:
+        user_temp = float(user_temp)
+    except ValueError:
+        print(f"{user_temp} is not an integer.")
+    else:
+        fahrenheit_convert = (9 / 5) * user_temp + 32
+        print(f"{user_temp}°C is equal to {fahrenheit_convert}°F")
+
+
+def main():
+    fahrenheit()
+
+
+if __name__ == "__main__":
+    main()
